5c075eacf08839f68164816614ee1c0cbc401dde
asseke01 <asylzhan12@mail.ru>
2024-10-18 23:37:40 +0600

    start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {StartPageComponent} from './components/start-page/start-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'', component:StartPageComponent
+  }
+
+];

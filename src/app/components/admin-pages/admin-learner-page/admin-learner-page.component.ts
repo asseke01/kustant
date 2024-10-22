ad3fff6c636e173fbccdb502210a542cc66cb48d
asseke01 <asylzhan12@mail.ru>
2024-10-22 18:06:57 +0500

    fix styles / add pages for admin

--- src/app/components/admin-pages/admin-learner-page/admin-learner-page.component.ts
+++ src/app/components/admin-pages/admin-learner-page/admin-learner-page.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, inject, TemplateRef, ViewChild} from '@angular/core';
+import {MatDialog, MatDialogActions, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {NgForOf} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-admin-learner-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogTitle,
+    MatIcon,
+    MatIconButton,
+    NgForOf,
+    ReactiveFormsModule
+  ],
   templateUrl: './admin-learner-page.component.html',
   styleUrl: './admin-learner-page.component.css'
 })
 export class AdminLearnerPageComponent {
+  @ViewChild('dialogTemplate') dialogTemplate!: TemplateRef<any>;
+  dialog = inject(MatDialog);
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(this.dialogTemplate, {
+      width: '90%',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
+  closeDialog(): void {
+    this.dialog.closeAll();
+  }
 
 }

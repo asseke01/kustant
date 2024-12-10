0a8deee554cea6197235a6f3abc18996ebd2adac
asseke01 <asylzhan12@mail.ru>
2024-12-10 21:52:52 +0600

    corp-main-page

--- src/app/components/user-pages/corp-main-page/corp-main-page.component.ts
+++ src/app/components/user-pages/corp-main-page/corp-main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-corp-main-page',
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './corp-main-page.component.css'
 })
 export class CorpMainPageComponent {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const schoolCards = document.querySelectorAll('.school-card');
+    schoolCards.forEach((card) => {
+      const rect = card.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 300 && rect.bottom > 0) {
+        card.classList.add('visible');
+      }
+    });
 
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+      const rect = card.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 100 && rect.bottom > 0) {
+        setTimeout(() => {
+          card.classList.add('visible-card');
+        }, index * 200);
+      }
+    });
+  }
 }

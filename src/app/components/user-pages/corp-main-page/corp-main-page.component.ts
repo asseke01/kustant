38fa9d286d0640392018e71519e56cd3795301c5
asseke01 <asylzhan12@mail.ru>
2024-12-20 18:17:16 +0500

    fix styles

--- src/app/components/user-pages/corp-main-page/corp-main-page.component.ts
+++ src/app/components/user-pages/corp-main-page/corp-main-page.component.ts
@@ -1,9 +1,12 @@
 import {Component, HostListener} from '@angular/core';
+import {UserFooterComponent} from '../user-footer/user-footer.component';
 
 @Component({
   selector: 'app-corp-main-page',
   standalone: true,
-  imports: [],
+  imports: [
+    UserFooterComponent
+  ],
   templateUrl: './corp-main-page.component.html',
   styleUrl: './corp-main-page.component.css'
 })
@@ -18,10 +21,28 @@ export class CorpMainPageComponent {
       }
     });
 
+    const testCards = document.querySelectorAll('.test-card');
+    testCards.forEach((card) => {
+      const rect = card.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 200 && rect.bottom > 0) {
+        card.classList.add('visible');
+      }
+    });
+
     const cards = document.querySelectorAll('.card');
     cards.forEach((card, index) => {
       const rect = card.getBoundingClientRect();
       if (rect.top < window.innerHeight - 100 && rect.bottom > 0) {
+        setTimeout(() => {
+          card.classList.add('visible-card');
+        }, index * 200);
+      }
+    });
+
+    const recordCard = document.querySelectorAll('.records-card');
+    recordCard.forEach((card, index) => {
+      const rect = card.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 150 && rect.bottom > 0) {
         setTimeout(() => {
           card.classList.add('visible-card');
         }, index * 200);
